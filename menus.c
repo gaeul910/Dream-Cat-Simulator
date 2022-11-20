04b1b6fd25b0df99064f6b4b5cdcf6f42e65b93f
gaeul910 <chw910@chungbuk.ac.kr>
2022-11-20 20:33:17 +0900

    DisplayModules 화면 출력 깜빡임 수정 Fixes #1

--- menus.c
+++ menus.c
@@ -7,11 +7,14 @@ int interaction(ItemList *list, Status *stats, PlayerData *playerdat)
     randomimg = getRandomCharacterNum(stats); // 현재 상태에 의거한 랜덤 이미지 번호 뽑아오기
     while (1)
     {
-        printStatus(list, stats, playerdat, NULL);
-        printCharacter(&randomimg, stats);
-        // 여기서부터 키 입력 메뉴 출력 & 입력 받기
-        // 반드시 while문 마지막에 스크린 모두 지우기 실행
-        printf("[F] 먹이주기    ");
+        if (StatUpdate(stats) == 1)
+        {
+            printStatus(list, stats, playerdat, NULL);
+            printCharacter(&randomimg, stats);
+            // 여기서부터 키 입력 메뉴 출력 & 입력 받기
+            // 반드시 while문 마지막에 스크린 모두 지우기 실행
+            printf("[F] 먹이주기    ");
+        }
         if (kbhit() == 1)
         {
             input = getch();
@@ -24,12 +27,14 @@ int interaction(ItemList *list, Status *stats, PlayerData *playerdat)
                 continue;
             }
         }
+        Sleep(2000);
         system("cls");
     }
     return 0;
 }
 int mainMenu(ItemList *list, Status *stats, PlayerData *playerdat)
 {
+    Status *oldStats = (Status *)malloc(sizeof(Status));
     char input;
 
     getCurrentStat(stats->laststatcode, stats); // 현재의 상태코드를 laststat에 입력시켜줌 (아직 개발중인 모듈임)
@@ -37,15 +42,19 @@ int mainMenu(ItemList *list, Status *stats, PlayerData *playerdat)
     randomimg = getRandomCharacterNum(stats); // 현재 상태에 의거한 랜덤 이미지 번호 뽑아오기
     while (1)
     {
-        printStatus(list, stats, playerdat, NULL);
-        printCharacter(&randomimg, stats);
-        // 여기서부터 키 입력 메뉴 출력 & 입력 받기
-        // 반드시 while문 마지막에 스크린 모두 지우기 실행
-        printf("[E] 인벤토리");
-        printf("[M] 지도 열기");
-        printf("[F] 상호 작용");
-        printf("[S] 저장하기");
-        printf("[O] 옵션");
+        if (StatUpdate(stats) == 1)
+        {
+            printStatus(list, stats, playerdat, NULL);
+            printCharacter(&randomimg, stats);
+            // 여기서부터 키 입력 메뉴 출력 & 입력 받기
+            // 반드시 while문 마지막에 스크린 모두 지우기 실행
+            printf("[E] 인벤토리");
+            printf("[M] 지도 열기");
+            printf("[F] 상호 작용");
+            printf("[S] 저장하기");
+            printf("[O] 옵션");
+            printf("%d", randomimg);
+        }
         if (kbhit() == 1)
         {
             input = getch();
@@ -71,8 +80,8 @@ int mainMenu(ItemList *list, Status *stats, PlayerData *playerdat)
                 continue;
             }
         }
-        gotoxy(0, 0);
-        Sleep(3000);
+        Sleep(2000);
+        system("cls");
     }
     return 0;
 }
