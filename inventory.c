b92c14e67aedd2f4ebb19c9b59de1041fcec2698
Choi-Eunrak <eunrak46@naver.com>
2022-11-23 21:35:32 +0900

    인벤토리가 알아서 꺼지던 현상을 수정했습니다.

--- inventory.c
+++ inventory.c
@@ -1,6 +1,5 @@
 int seeinventory(ItemList *itemlist)
 {
-    system("cls");
     typedef struct Inventory
     {
         item inventoryitem[256];
@@ -9,13 +8,10 @@ int seeinventory(ItemList *itemlist)
     Inventory *inventory = (Inventory *)malloc(sizeof(Inventory));
 
     char input;
+    int MAXpage = 0;
     int k = 0;
-    int page = k / 10;
-    int MAXpage = (inventory->inventorycount / 10) + 1;
-    inventory->inventorycount = 0;
-    printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-    printf("║                         인벤토리 ( 1 / %d )                           ║\n", MAXpage);
-    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    system("cls");
+
     for (int i = 0; i < itemlist->itemcount; i++)
     {
         if (itemlist->itemArr[i].amount != 0)
@@ -28,89 +24,109 @@ int seeinventory(ItemList *itemlist)
             // printf("%s : %d\n", itemlist->itemArr[i].name, itemlist->itemArr[i].amount);
         }
     }
-    for (int k = 0; k < inventory->inventorycount; k++)
+    MAXpage = 1 + (inventory->inventorycount / 10);
+    int page = k / 10;
+    k = 0;
+    printf("\n");
+    printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                         인벤토리 ( 1 / %d )                           ║\n", MAXpage);
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    while (1)
     {
-        printf("%s : %d\n", inventory->inventoryitem[k].name, inventory->inventoryitem[k].amount);
-        if (page == 1)
+        for (k; k < inventory->inventorycount; k++)
         {
-            printf("[H] 다음 페이지");
-            if (kbhit() == 1)
+            printf("%s : %d\n", inventory->inventoryitem[k].name, inventory->inventoryitem[k].amount);
+            if (k % 9 == 0 || k == inventory->inventorycount)
             {
-                input = getch();
-                if (input == 'h')
+                if (k <= 9 && inventory->inventorycount >= 9)
                 {
-                    system("cls");
-                    printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-                    printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
-                    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+                    printf("[H] 다음 페이지");
+                    if (kbhit() == 1)
+                    {
+                        input = getch();
+                        if (input == 'h')
+                        {
+                            system("cls");
+                            printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+                            printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
+                            printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+                        }
+                        else
+                        {
+                            printf("다시 입력해주세요.");
+                            input = getch();
+                        }
+                    }
+                }
+                // else if (k == inventory->inventorycount && page == 0)
+                // {
+                //     system("pause");
+                // }
+                else if (k == inventory->inventorycount)
+                {
+                    printf("[G] 이전 페이지");
+                    while (1)
+                    {
+                        if (kbhit() == 1)
+                        {
+                            input = getch();
+                            if (input == 'g')
+                            {
+                                k = k - 10 - (k % 10);
+                                system("cls");
+                                printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+                                printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
+                                printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+                                break;
+                            }
+                            else
+                            {
+                                printf("다시 입력해주세요.");
+                            }
+                        }
+                    }
+                }
+                else if (page == 0 && inventory->inventorycount <= 10)
+                {
+                    printf("");
                 }
                 else
                 {
-                    printf("다시 입력해주세요.");
-                    input = getch();
-                }
-            }
-        }
-        else if (k == inventory->inventorycount)
-        {
-            printf("[G] 이전 페이지");
-            while (1)
-            {
-                if (kbhit() == 1)
-                {
-                    input = getch();
-                    if (input == 'g')
+                    printf("[G] 이전 페이지    [H] 다음 페이지");
+                    while (1)
                     {
-                        k = k - 10 - (k % 10);
-                        system("cls");
-                        printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-                        printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
-                        printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-                        break;
-                    }
-                    else
-                    {
-                        printf("다시 입력해주세요.");
-                    }
-                }
-            }
-        }
-        else if (page == 0)
-        {
-            printf("");
-        }
-        else
-        {
-            printf("[G] 이전 페이지    [H] 다음 페이지");
-            while (1)
-            {
-                if (kbhit() == 1)
-                {
-                    input = getch();
-                    if (input == 'g')
-                    {
-                        k = k - 20;
-                        system("cls");
-                        printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-                        printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
-                        printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-                        break;
-                    }
-                    else if (input == 'h')
-                    {
-                        system("cls");
-                        printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-                        printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
-                        printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-                        break;
-                    }
-                    else
-                    {
-                        printf("다시 입력해주세요.\n");
+                        if (kbhit() == 1)
+                        {
+                            input = getch();
+                            if (input == 'g')
+                            {
+                                k = k - 20;
+                                system("cls");
+                                printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+                                printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
+                                printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+                                break;
+                            }
+                            else if (input == 'h')
+                            {
+                                system("cls");
+                                printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+                                printf("║                         인벤토리 ( %d / %d )                          ║\n", page, MAXpage);
+                                printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+                                break;
+                            }
+                            else if (input == 'h')
+                                break;
+                            else
+                            {
+                                printf("다시 입력해주세요.\n");
+                            }
+                        }
                     }
                 }
             }
         }
+        // Sleep(1000);
     }
     free(inventory);
 }
