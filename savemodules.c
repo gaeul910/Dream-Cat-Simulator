cbc55e0254789a7ae2dac9fb67c6950009f91d3a
gaeul910 <chw910@chungbuk.ac.kr>
2022-11-27 18:03:10 +0900

    Fixed initGame file print

--- savemodules.c
+++ savemodules.c
@@ -1,40 +1,3 @@
-int initGame(ItemList *items, Status *stats, PlayerData *playerdat)
-{
-    char temp[128];
-    FILE *er = fopen("./savedata/playerinfo.txt", "r");
-    if ((er = fopen("./savedata/playerinfo.txt", "r")) == NULL)
-    {
-        fclose(er);
-        FILE *fp = fopen("./savedata/playerinfo.txt", "w"); // 파일을 쓰기로 열기
-        printf("╔══════════════════════════════════════════════════════════════════════╗\n");
-        printf("║             Dream-Cat-Simulator에 오신 것을 환영합니다!              ║\n");
-        printf("╚══════════════════════════════════════════════════════════════════════╝\n\n");
-        printf("사용자 이름을 입력해주세요!\n");
-        gets(temp);
-        strcpy(playerdat->playerName, temp);
-        fprintf(fp, "PlayerName=%s", temp);
-        printf("개냥이 이름을 입력해주세요!\n");
-        gets(temp);
-        strcpy(playerdat->dreamCatName, temp);
-        fprintf(fp, "DreamCatName=%s", temp);
-        fclose(fp);
-        stats->hunger = 1000;
-        stats->health = 1000;
-        stats->delight = 500;
-        stats->normal = 750;
-        stats->sadness = 0;
-        stats->anger = 0;
-        stats->friendship = 0;
-        system("cls");
-        return 1;
-    }
-    else
-    {
-        fclose(er);
-        return 0;
-    }
-}
-
 int Check_StatName(char *StatName, int StatAmount)
 {
     if (strcmp("hunger", StatName) == 0)
@@ -230,6 +193,44 @@ int loadGame(ItemList *items, Status *stats, PlayerData *playerdat)
     stats->updatetime = time(NULL); // 추후 흘러간 시간 계산 후 마지막에 수행
 
     return 0;
+}
+
+int initGame(ItemList *items, Status *stats, PlayerData *playerdat)
+{
+    char temp[128];
+    FILE *er = fopen("./savedata/playerinfo.txt", "r");
+    if ((er = fopen("./savedata/playerinfo.txt", "r")) == NULL)
+    {
+        fclose(er);
+        FILE *fp = fopen("./savedata/playerinfo.txt", "w"); // 파일을 쓰기로 열기
+        printf("╔══════════════════════════════════════════════════════════════════════╗\n");
+        printf("║             Dream-Cat-Simulator에 오신 것을 환영합니다!              ║\n");
+        printf("╚══════════════════════════════════════════════════════════════════════╝\n\n");
+        printf("사용자 이름을 입력해주세요!\n");
+        fgets(temp, sizeof(temp), stdin);
+        strcpy(playerdat->playerName, temp);
+        fprintf(fp, "PlayerName=%s", temp);
+        printf("개냥이 이름을 입력해주세요!\n");
+        fgets(temp, sizeof(temp), stdin);
+        strcpy(playerdat->dreamCatName, temp);
+        fprintf(fp, "DreamCatName=%s", temp);
+        fclose(fp);
+        stats->hunger = 1000;
+        stats->health = 1000;
+        stats->delight = 500;
+        stats->normal = 750;
+        stats->sadness = 0;
+        stats->anger = 0;
+        stats->friendship = 0;
+        saveGame(items, stats);
+        system("cls");
+        return 1;
+    }
+    else
+    {
+        fclose(er);
+        return 0;
+    }
 }
 
 int gamereset()
