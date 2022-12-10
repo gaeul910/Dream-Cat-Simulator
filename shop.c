19e85a76b307fa7efe50c7464f38e7fc9e26e494
1Parksanghyun <nks168810@gmail.com>
2022-12-10 19:15:48 +0900

    상점 기능 버그 수정

--- shop.c
+++ shop.c
@@ -21,10 +21,10 @@ void shop_food(ItemList *itemlist)
 {
     eraser(7, 13);
     gotoxy(0, 7);
-    printf(" 먹이 상점\n\n");
+    printf(" 먹이다옹~\n\n");
     for (int i = 1; i < 4; i++)
     {
-        printf(" o %s %d\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
+        printf(" o %s %dG\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
     }
 
     int num = cursor(8, 14, 2);
@@ -60,10 +60,10 @@ void shop_snack(ItemList *itemlist)
 {
     eraser(7, 13);
     gotoxy(0, 7);
-    printf(" 간식 상점\n\n");
+    printf(" 간식이다옹~\n\n");
     for (int i = 4; i < 6; i++)
     {
-        printf(" o %s %d\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
+        printf(" o %s %dG\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
     }
 
     int num = cursor(8, 12, 2);
@@ -94,10 +94,10 @@ void shop_toy(ItemList *itemlist)
 {
     eraser(7, 13);
     gotoxy(0, 7);
-    printf(" 간식 상점\n\n");
+    printf(" 장난감이다옹~\n\n");
     for (int i = 6; i < 9; i++)
     {
-        printf(" o %s %d\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
+        printf(" o %s %dG\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
     }
 
     int num = cursor(8, 14, 2);
@@ -120,6 +120,39 @@ void shop_toy(ItemList *itemlist)
         shop_calcul(itemlist, 8);
     }
 
+    default:
+        gotoxy(0, 15);
+        break;
+    }
+
+    system("pause");
+}
+
+void shop_walk(ItemList *itemlist)
+{
+    eraser(7, 13);
+    gotoxy(0, 7);
+    printf(" 산책에 필수다옹~\n\n");
+    for (int i = 9; i < 11; i++)
+    {
+        printf(" o %s %dG\n\n", itemlist->itemArr[i].name, itemlist->itemArr[i].price);
+    }
+
+    int num = cursor(8, 12, 2);
+
+    switch (num)
+    {
+    case 9:
+    {
+        shop_calcul(itemlist, 9);
+        break;
+    }
+    case 11:
+    {
+
+        shop_calcul(itemlist, 10);
+        break;
+    }
     default:
         gotoxy(0, 15);
         break;
@@ -171,7 +204,8 @@ void shop(ItemList *itemlist)
             shop_toy(itemlist);
             break;
         case 13:
-            printf("you select item3\n");
+            eraser(7, 10);
+            shop_walk(itemlist);
             break;
 
         default:
