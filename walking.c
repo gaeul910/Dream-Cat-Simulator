7b31b9aeacb79a362818482781509ba608dc42d1
Choi-Eunrak <eunrak46@naver.com>
2022-12-13 12:57:01 +0900

    디테일 추가

--- walking.c
+++ walking.c
@@ -16,7 +16,7 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
             {
                 printf("걸렸다!\n\n");
                 Sleep(2000);
-                printf("-700G\n\n");
+                printf("[-] 700G\n\n");
                 itemlist->gold -= 700;
             }
             else
@@ -27,7 +27,7 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
             printf("배변봉투로 잘 마무리 했다.\n\n");
             addItem(10, -1, itemlist);
             Sleep(2000);
-            printf("- 배변봉투\n\n");
+            printf("[-] 배변봉투\n\n");
         }
         break;
     case 1:
@@ -36,13 +36,13 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
         {
             printf("목줄을 채우지 않은 것을 걸려버렸다..\n\n");
             Sleep(2000);
-            printf("-1000G\n\n");
+            printf("[-] 1000G\n\n");
             itemlist->gold -= 1000;
         }
         else
         {
             printf("위험할뻔 했지만 목줄로 잘 통제했다.\n\n");
-            printf("+200 friendship\n\n");
+            printf("[+] 200 friendship\n\n");
             status->friendship = calcStatsValue(status->friendship, 200);
         }
         break;
@@ -51,7 +51,7 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
         Sleep(2000);
         printf("이겼다!!\n\n");
         Sleep(2000);
-        printf("+300 Delight\n\n");
+        printf("[+] 300 Delight\n\n");
         status->delight = calcStatsValue(status->delight, 300);
         break;
     case 3:
@@ -59,7 +59,7 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
         Sleep(2000);
         printf("져버렸다..\n\n");
         Sleep(2000);
-        printf("+150 Anger\n\n");
+        printf("[+] 150 Anger\n\n");
         status->anger = calcStatsValue(status->anger, 150);
         break;
     case 4:
@@ -71,38 +71,38 @@ int event(ItemList *itemlist, Status *status, PlayerData *playerdat)
             printf("돈을 주웠다!\n\n");
             itemlist->gold += 500;
             Sleep(2000);
-            printf("+500 G\n\n");
+            printf("[+] 500 G\n\n");
             break;
         case 1:
             printf("아이템 '무법자'를 얻었다!\n\n");
             addItem(11, 1, itemlist);
             Sleep(2000);
-            printf("+ 무법자");
+            printf("[+] 무법자");
             break;
         case 2:
             printf("아이템 '부활'을 얻었다!\n\n");
             addItem(12, 1, itemlist);
             Sleep(2000);
-            printf("+ 부활");
+            printf("[+] 부활");
             break;
         }
         break;
     case 5:
         printf("갑자기 비가와서 산책을 그만해야하는 %s는 슬프다..\n\n", playerdat->dreamCatName);
         Sleep(2000);
-        printf("+200 Sadness\n\n");
+        printf("[+] 200 Sadness\n\n");
         status->sadness = calcStatsValue(status->sadness, 200);
         break;
     case 6:
         printf("%s이(가) 산책을 하다가 넘어졌다..\n\n", playerdat->dreamCatName);
         Sleep(2000);
-        printf("- Health\n\n");
+        printf("[-] Health\n\n");
         status->health = calcStatsValue(status->health, -1);
         break;
     case 7:
         printf("%s이(가) 산책을 하다가 귀엽다고 칭찬받았다!\n\n", playerdat->dreamCatName);
         Sleep(2000);
-        printf("+200 Delight\n\n");
+        printf("[+] 200 Delight\n\n");
         status->delight = calcStatsValue(status->delight, 200);
         break;
     }
